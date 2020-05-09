85cb66ccf4c042ab40be5b0dda3f74d3721f6df3
Claudio Aragao Tessaro <claudio.tessaro@ctis.com.br>
2020-05-08 21:51:05 -0300

    Finalizando a estilização dos componentes

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,7 +1,66 @@
 import React from "react";
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from "./styles";
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from "react-icons/md";
 
 export default function cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-olympikus-day-feminino/12/D22-2026-012/D22-2026-012_zoom2.jpg?ts=1571405177&ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tenis</strong>
+              <span>R$129,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="buton">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1111,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,101 @@
 import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-// import { Container } from './styles';
+import { ProductList } from "./styles";
 
 export default function home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-day-feminino/12/D22-2026-012/D22-2026-012_zoom2.jpg?ts=1571405177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-day-feminino/12/D22-2026-012/D22-2026-012_zoom2.jpg?ts=1571405177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-day-feminino/12/D22-2026-012/D22-2026-012_zoom2.jpg?ts=1571405177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-day-feminino/12/D22-2026-012/D22-2026-012_zoom2.jpg?ts=1571405177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-day-feminino/12/D22-2026-012/D22-2026-012_zoom2.jpg?ts=1571405177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-day-feminino/12/D22-2026-012/D22-2026-012_zoom2.jpg?ts=1571405177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
